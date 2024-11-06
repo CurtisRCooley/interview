0005111d2e2c66f69d2dc15dce30d48a1cc8eaf7
Curtis R Cooley <curtiscooley@gmail.com>
2024-11-06 14:43:34 -0700

    Initial commit

---
+++ src/test/java/com/curtisrcooley/interview/InterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curtisrcooley.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
