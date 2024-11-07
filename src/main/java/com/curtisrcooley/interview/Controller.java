b91e473abed5768a75e96d96526ba1f32f518bd5
Curtis R Cooley <curtiscooley@gmail.com>
2024-11-06 17:31:26 -0700

    Parse the JSON

---
+++ src/main/java/com/curtisrcooley/interview/Controller.java
@@ -0,0 +1,28 @@
+package com.curtisrcooley.interview;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/apiv1")
+public class Controller {
+    private final ObjectMapper objectMapper;
+
+    public Controller(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @PostMapping("person")
+    public void person(@RequestBody String json) {
+        try {
+            Person p = objectMapper.readValue(json, Person.class);
+            System.out.println(p.getName() + " " + p.getPhone() + " " + p.getAge());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/com/curtisrcooley/interview/SpringApplication.java
+++ src/main/java/com/curtisrcooley/interview/SpringApplication.java
@@ -1,7 +1,9 @@
 package com.curtisrcooley.interview;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringApplication {
 
