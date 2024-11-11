dedf85b1196096f853f1b0389e442f7f3e101d98
Curtis R Cooley <curtiscooley@gmail.com>
2024-11-11 12:12:26 -0700

    Removes unnecessary annotations

--- src/main/java/com/curtisrcooley/interview/Controller.java
+++ src/main/java/com/curtisrcooley/interview/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/apiv1")
+@RequestMapping("/api_v1")
 public class Controller {
     private final ObjectMapper objectMapper;
 
--- src/main/java/com/curtisrcooley/interview/SpringApplication.java
+++ src/main/java/com/curtisrcooley/interview/SpringApplication.java
@@ -1,9 +1,7 @@
 package com.curtisrcooley.interview;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class SpringApplication {
 
